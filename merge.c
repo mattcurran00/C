2d4b41511fd0952f9c51f9450105b53c767e8f45
Matthew Curran <mattcurran1212@gmail.com>
2026-08-21 11:01:37 +0100

    merge sort finished for integers anyways

--- merge.c
+++ merge.c
@@ -2,7 +2,7 @@
 
 
 
-int merge();
+void merge(int *left, int leftsize, int *right, int rightsize, int *result, int length);
 void mergeSort(int *numbers, int length);
 
 int main(){
@@ -11,33 +11,81 @@ int main(){
 
     int length = sizeof(numbers) / sizeof(numbers[0]);
     mergeSort(numbers, length);
+
+     fflush(stdout);
+    printf("\nSorting Finished. Sorted array is: ");
+    for(int i = 0; i < length; i++){
+        printf("%d, ", numbers[i]);
+    }
     return 0;
 }
 
-int merge(){
-    return 0;
+void merge(int *left,int leftsize, int *right, int rightsize, int *result, int length){
+    
+    int leftIndex = 0;
+    int rightIndex = 0;
+    int resultIndex = 0; 
 
+    while(leftIndex < leftsize && rightIndex < rightsize){
+        if(left[leftIndex] <= right[rightIndex]){
+            result[resultIndex] = left[leftIndex];
+            leftIndex++;
+        }
+        else{
+            if(left[leftIndex] >= right[rightIndex]){
+                result[resultIndex] = right[rightIndex];
+                rightIndex++;
+            }
+        }
+        resultIndex++;
+    }
+
+    while(leftIndex < leftsize){
+        result[resultIndex] = left[leftIndex];
+        resultIndex++;
+        leftIndex++;
+    }
+    while(rightIndex < rightsize){
+        result[resultIndex] = right[rightIndex];
+        resultIndex++;
+        rightIndex++;
+    }
 }
 
 void mergeSort(int *numbers, int length){
 
-    if(length <= 1){
+    if(length <= 1){ //base case to stop the infinite loop 
         return;
     }
     int mid = length / 2;
+
+    int leftlength = mid;
+    int rightlength = length - mid;
     printf("\nMid = %d \n", mid);
 
-    int temp[mid];
-    int tlength = sizeof(temp) / sizeof(temp[0]);
+   //temparrays 
+   int left[leftlength];
+   int right[rightlength];
 
     for (int i = 0; i < mid; i++){
-        temp[i] = numbers[i];
+        left[i] = numbers[i];
+    }
+
+    for(int i = 0; i < rightlength; i++){
+        right[i] = numbers[mid + i];
     }
 
     for (int i = 0; i < mid; i++){
         fflush(stdout);
-        printf("\nLeft half is: %d", temp[i]);
+        printf("\nLeft half is: %d", left[i]);
+    }
+    for (int i = 0; i < rightlength; i++){
+        fflush(stdout);
+        printf("\nRight half is: %d", right[i]);
     }
 
-    mergeSort(temp, tlength);
+    mergeSort(left, leftlength);
+    mergeSort(right, rightlength);
+
+    merge(left, leftlength, right, rightlength, numbers, length);
 }
